6ca12e16c8686e7c0a182e4f709fd8d8da292be8
LangTzyy <gilanghermawan2807@gmail.com>
2025-10-11 22:09:08 +0700

    Update models.py

--- warga/models.py
+++ warga/models.py
@@ -9,3 +9,12 @@ class Warga(models.Model):
 
     def __str__(self):
         return self.nama_lengkap
+
+class Laporan(models.Model):
+    warga = models.ForeignKey(Warga, on_delete=models.CASCADE, related_name='laporan')
+    judul = models.CharField(max_length=200)
+    isi_laporan = models.TextField()
+    tanggal_laporan = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.judul} - {self.warga.nama_lengkap}"
